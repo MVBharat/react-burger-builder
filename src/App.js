cc9561f4ac734a482f75b4f25427656910ab0dd3
MVBharat <manebharat85@gmail.com>
2019-01-29 18:03:07 +0530

    Add  Burger Ingredients components and proptypes validation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <h1>hello</h1>  
+          <BurgerBuilder />
         </Layout>        
       </div>
     );
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aux from '../../hoc/Aux'
+
+
+class BurgerBuilder extends React.Component{
+    render(){
+        return(
+            <Aux>
+                <p>Burger</p>
+                <p>Build controls</p>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder
