d023c5b1c398f3f17516463871553c500e6d6d0a
MVBharat <manebharat85@gmail.com>
2019-01-29 17:22:36 +0530

    add components and auxolury

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Aux from '../../hoc/Aux'
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar, Sidebar, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+
+)
+
+export default layout
