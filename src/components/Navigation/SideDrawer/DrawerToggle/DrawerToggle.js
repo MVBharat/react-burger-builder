412c6106e22b7835c44075b2bee98b1693813524
MVBharat <manebharat85@gmail.com>
2019-01-31 13:20:54 +0530

    Add Hamberger for sidebar drawer

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import classes from './DrawerToggle.css'
 
 const drawerToggle = (props) => (
 
-    <div onClick={props.clicked}>
-        Menu
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 )
 
