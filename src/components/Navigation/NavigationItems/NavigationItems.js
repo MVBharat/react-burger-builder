c3133bfa674f48fae6cb55948909449eb8351c85
MVBharat <manebharat85@gmail.com>
2019-01-31 10:46:10 +0530

    fix navigation items issue

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import NavigationItem from './NavigationItem/NavigationItem'
 const navigationItems = () => (
     <ul className={classes.NavigationItems}> 
         <NavigationItem link="/" active >Burger Builder</NavigationItem>
-        <NavigationItem link="/" >Checkout</NavigationItem>
+        <NavigationItem link="/"  >Checkout</NavigationItem>
     </ul>
 )
 
