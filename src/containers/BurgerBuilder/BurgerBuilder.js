e70ec62f5900c6032021d454d246614715bed64c
MVBharat <manebharat85@gmail.com>
2019-02-06 16:57:27 +0530

    implemented burger checkout component successfully

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -82,7 +82,17 @@ class BurgerBuilder extends React.Component{
         //             this.setState({loading: false, purchasing: false})
         //         })
 
-        this.props.history.push('/checkout');
+        const queryParams = []
+        for (let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i])) // property name = value for property name
+        }
+
+        const queryString = queryParams.join('&')
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     addIngredientHandler = (type) =>{
