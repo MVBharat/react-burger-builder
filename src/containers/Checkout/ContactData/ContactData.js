7176ec7327302cf8a3fd4ce6211297aa2d1b896d
MVBharat <manebharat85@gmail.com>
2019-02-07 17:48:50 +0530

    Add Validation form design by CSS and validateChecking method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,6 +19,7 @@ class ContactData extends React.Component{
                     required: true,
                 },
                 valid: false,
+                touched: false,
             },
             street: {
                 elementType: 'input',
@@ -31,6 +32,7 @@ class ContactData extends React.Component{
                     required: true,
                 },
                 valid: false,
+                touched: false,
             },
             zipCode: {
                 elementType: 'input',
@@ -41,10 +43,11 @@ class ContactData extends React.Component{
                 value: '',
                 validation: {
                     required: true,
-                    minLength: 5,
-                    maxLength: 5,
+                    minLength: 6,
+                    maxLength: 6,
                 },
                 valid: false,
+                touched: false,
             },
             country: {
                 elementType: 'input',
@@ -57,6 +60,7 @@ class ContactData extends React.Component{
                     required: true,
                 },
                 valid: false,
+                touched: false,
             },
             email: {
                 elementType: 'input', 
@@ -69,6 +73,7 @@ class ContactData extends React.Component{
                     required: true,
                 },
                 valid: false,
+                touched: false,
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -139,6 +144,7 @@ class ContactData extends React.Component{
         } 
         updatedFormElement.value = event.target.value;
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+        updatedFormElement.touched = true
         updatedOrderForm[inputIdentifier] = updatedFormElement;
         console.log(updatedFormElement);
         this.setState({orderForm: updatedOrderForm })
@@ -162,6 +168,9 @@ class ContactData extends React.Component{
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}
                     />
                 ))}
