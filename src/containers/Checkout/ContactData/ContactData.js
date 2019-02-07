b08373514347f464bcaab27589bd5ced0a3bfa43
MVBharat <manebharat85@gmail.com>
2019-02-07 17:23:42 +0530

    Add Validation to ContactData form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,11 @@ class ContactData extends React.Component{
                     type: 'text',   // html type without tag
                     placeholder: 'Your name'    // html placeholder without tag
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                },
+                valid: false,
             },
             street: {
                 elementType: 'input',
@@ -22,7 +26,11 @@ class ContactData extends React.Component{
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                },
+                valid: false,
             },
             zipCode: {
                 elementType: 'input',
@@ -30,7 +38,13 @@ class ContactData extends React.Component{
                     type: 'text',
                     placeholder: 'PIN Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5,
+                },
+                valid: false,
             },
             country: {
                 elementType: 'input',
@@ -38,7 +52,11 @@ class ContactData extends React.Component{
                     type: 'text',
                     placeholder: 'Your Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                },
+                valid: false,
             },
             email: {
                 elementType: 'input', 
@@ -46,7 +64,11 @@ class ContactData extends React.Component{
                     type: 'email',
                     placeholder: 'Your E-mail'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                },
+                valid: false,
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -88,6 +110,26 @@ class ContactData extends React.Component{
                 })
     }
 
+
+    checkValidity(value, rules){
+        
+        let isValid = true;
+
+        if(rules.required){
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        return isValid
+    }
+
     inputChangedHandler = (event, inputIdentifier) =>{
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -96,7 +138,9 @@ class ContactData extends React.Component{
             ...updatedOrderForm[inputIdentifier]
         } 
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
         this.setState({orderForm: updatedOrderForm })
     }
 
