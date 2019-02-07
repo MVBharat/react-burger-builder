d133c7253b360c3228f09363aee58d7ba14b54c0
MVBharat <manebharat85@gmail.com>
2019-02-07 16:12:12 +0530

    add dynamic submit button

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -66,9 +66,16 @@ class ContactData extends React.Component{
         e.preventDefault();
 
         this.setState({ loading: true })
+
+        const formData ={};
+        for(let formElementIdentifier in this.state.orderForm ){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,            
+            orderData: formData
         }
 
         axios.post('/orders.json', order)
@@ -104,7 +111,7 @@ class ContactData extends React.Component{
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler} >
                 {formElementsArray.map(formElement =>(
                     <Input 
                         key={formElement.id}
@@ -114,7 +121,7 @@ class ContactData extends React.Component{
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}
                     />
                 ))}
-                <Button btnType="Success" clicked={this.orderHandler}  >ORDER</Button>
+                <Button btnType="Success" >ORDER</Button>
             </form>
         );
         if(this.state.loading){
